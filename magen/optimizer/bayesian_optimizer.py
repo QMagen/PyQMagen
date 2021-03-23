55022f5d38da8c1601ebbdd177ff195e5ef9b60c
bchen <bunbun.d.chan@gmail.com>
2021-03-23 22:22:11 +0800

    opt.minimize

--- magen/optimizer/bayesian_optimizer.py
+++ magen/optimizer/bayesian_optimizer.py
@@ -39,22 +39,32 @@ class BayesianOptimizer(Optimizer):
         if custom_loss_func:
             self.eval_loss = custom_loss_func
 
+    def printHeader(self):
+        output_line = '|{:^6s}|{:^8s}|'.format('Itr', 'Loss')
+        for i in range(len(self.parameter_space)):
+            output_line += '{:^10s}|'.format(list(self.parameter_space.keys())[i])
+        output_line += '|{:^6s}|{:^8s}|'.format('BestIt','BestLoss')
+        for i in range(len(self.parameter_space)):
+            output_line += 'Best{:^6s}|'.format(list(self.parameter_space.keys())[i])
+
+        print('-'*100)
+        print(output_line)
+        print('-'*100)
+
+
     def minimize(self, log_accelerate=True):
 
         utility = UtilityFunction(kind=self.acquisition_func, kappa=2.5, xi=0.0)
         result = BayesianOptimizerResult()
 
-        aux = '|{{:=^{}s}}|'.format(len(self.parameter_space) * 11 + 21)
+        aux = '|{{:=^{}s}}|'.format(len(self.parameter_space) * 11 + 65)
         print(aux.format('Optimization Start'))
-        output_line = '|{:^10s}|{:^10s}|'.format('Itr', 'Loss')
-        for i in range(len(self.parameter_space)):
-            output_line += '{:^10s}|'.format(list(self.parameter_space.keys())[i])
-        print(output_line)
-
-        if self.record_BO:
-            BO_record = []
+        
+        if self.record_BO: BO_record = []
 
         for i in range(self.n_exp_total):
+
+            if i%15==0: self.printHeader()
 
             if i < self.num_warm_up:
                 parameter_space_value = list(self.parameter_space.values())
@@ -100,10 +110,16 @@ class BayesianOptimizer(Optimizer):
                 loss_to_save = -loss.detach().numpy()
 
             self.loss_record[i] = loss_to_save
+            idx_min = self.loss_record[:i+1].argmin()
 
-            output_line = '|{:^10d}|{:^10.2e}|'.format(i, loss_to_save)
+            output_line = '|{:^6d}|{:^8.1e}|'.format(i, loss_to_save)
             for j in range(len(self.parameter_space)):
-                output_line += '{:^10.5f}|'.format(next_point[list(self.parameter_space.keys())[j]])
+                paraName = list(self.parameter_space.keys())[j]
+                output_line += '{:^10.5f}|'.format(next_point[paraName])
+            output_line += '|{:^6d}|{:^8.1e}|'.format(idx_min,self.loss_record[idx_min])
+            for j in range(len(self.parameter_space)):
+                paraName = list(self.parameter_space.keys())[j]
+                output_line += '{:^10.5f}|'.format(self.parameter_record[idx_min][paraName])
             print(output_line)
 
         result.parameter_record = self.parameter_record
@@ -115,9 +131,8 @@ class BayesianOptimizer(Optimizer):
         #     result.loss_record = -self.loss_record
 
         result.loss_record = self.loss_record
-        result.best_parameter = self.parameter_record[self.loss_record.argmin()]
-        result.best_loss = self.loss_record.min()
-
+        result.best_parameter = self.parameter_record[idx_min]
+        result.best_loss = self.loss_record[idx_min]
 
 
         print(aux.format('Optimization Finised'))
